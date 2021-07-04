9c8e6f791f1078b90e13604464d69b2b3c2fd136
franco.balich@gmail.com <franco.balich@gmail.com>
2021-07-03 23:41:06 -0300

    Add hook useEffect

--- Clase 2/giffy/src/App.js
+++ Clase 2/giffy/src/App.js
@@ -1,10 +1,29 @@
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
+const GIFS =[
+  'https://media.giphy.com/media/EPcvhM28ER9XW/giphy.gif',
+  'https://media.giphy.com/media/pO1H8mAU7geAw/giphy.gif'
+];
+const DIFFERNTS_GIFS=[
+  'https://media.giphy.com/media/ySVKduoNNFoRy/giphy.gif',
+  'https://media.giphy.com/media/3eTsBTBJaFvJC/giphy.gif'
+];
 function App() {
+  const [gifs, setGifs]=useState(GIFS);
+
+  useEffect(()=>{
+    console.log("Actualizando los gif");
+    setGifs(DIFFERNTS_GIFS);
+  },[]);
+
   return (
     <div className="App">
       <section className="App-content">
-        
+        {
+          gifs.map(singleGif=><img src={singleGif} />)
+        }
+        <button >Cambiar Gifs</button> 
       </section>
     </div>
   );
