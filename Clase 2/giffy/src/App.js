b720698a7d2401e37a89ea9ca23e78266f7b9ad2
franco.balich@gmail.com <franco.balich@gmail.com>
2021-07-05 00:07:51 -0300

    Title and id was added in the component

--- Clase 2/giffy/src/App.js
+++ Clase 2/giffy/src/App.js
@@ -1,22 +1,28 @@
-import React, {useEffect, useState} from 'react';
-import './App.css';
-import getGifs from './services/getGifs';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import getGifs from "./services/getGifs";
 
 function App() {
-  const [gifs, setGifs]=useState([]);
+  const [gifs, setGifs] = useState([]);
 
-  useEffect(()=>{
+  useEffect(() => {
     console.log("Actualizando los gif");
-    getGifs({keyword:'panda'}).then(gifs=> setGifs(gifs));
-  },[]);
+    getGifs({ keyword: "panda" }).then((gifs) => setGifs(gifs));
+  }, []);
 
   return (
     <div className="App">
       <section className="App-content">
-        {
-          gifs.map(singleGif=><img src={singleGif} />)
-        }
-        <button>Cambiar Gifs</button> 
+        {gifs.map((singleGif) => {
+          return (
+            <div>
+              <h4>{singleGif.title}</h4>
+              <p>{singleGif.id}</p>
+              <img src={singleGif.url} alt={singleGif.title} />
+            </div>
+          );
+        })}
+        <button>Cambiar Gifs</button>
       </section>
     </div>
   );
--- Clase 2/giffy/src/services/getGifs.js
+++ Clase 2/giffy/src/services/getGifs.js
@@ -1,15 +1,16 @@
-const apiKey='VBFqqszK3iB5zlGkAm0iooJjHWSu9rhb';
+const apiKey = "VBFqqszK3iB5zlGkAm0iooJjHWSu9rhb";
 
-
-
-export default function getGifs({keyword='robot'}){
-    const apiURL=`https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${keyword}&limit=25&offset=0&rating=g&lang=en`;
-    return fetch(apiURL)
-      .then(res=> res.json())
-      .then(response=>{
-        const {data=[]}=response;
-        const gifs = data.map(image=> image.images.downsized_medium.url);
-
-        return gifs
-      })
+export default function getGifs({ keyword = "robot" }) {
+  const apiURL = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${keyword}&limit=25&offset=0&rating=g&lang=en`;
+  return fetch(apiURL)
+    .then((res) => res.json())
+    .then((response) => {
+      const { data = [] } = response;
+      const gifs = data.map((image) => {
+        const { images, title, id } = image;
+        const { url } = image.images.downsized_medium;
+        return { title, id, url };
+      });
+      return gifs;
+    });
 }
