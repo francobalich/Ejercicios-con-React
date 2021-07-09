b93059ecfe2f2269e77700b58dca8c46faaf38b7
franco.balich@gmail.com <franco.balich@gmail.com>
2021-07-09 00:15:01 -0300

    Gif component was created

--- Clase 2/giffy/src/App.js
+++ Clase 2/giffy/src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
 import getGifs from "./services/getGifs";
-
+import Gif from "./components/Gif";
 function App() {
   const [gifs, setGifs] = useState([]);
 
@@ -13,15 +13,9 @@ function App() {
   return (
     <div className="App">
       <section className="App-content">
-        {gifs.map((singleGif) => {
-          return (
-            <div>
-              <h4>{singleGif.title}</h4>
-              <p>{singleGif.id}</p>
-              <img src={singleGif.url} alt={singleGif.title} />
-            </div>
-          );
-        })}
+        {gifs.map((singleGif) => 
+          <Gif title={singleGif.title} id={singleGif.id} url={singleGif.url} />
+        )}
         <button>Cambiar Gifs</button>
       </section>
     </div>
