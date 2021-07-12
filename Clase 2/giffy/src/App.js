5e0060ca9f65d82ed64e7e0f5c45893e7d294531
franco.balich@gmail.com <franco.balich@gmail.com>
2021-07-12 20:08:55 -0300

    Add css styles

--- Clase 2/giffy/src/App.js
+++ Clase 2/giffy/src/App.js
@@ -1,23 +1,20 @@
 import React, { useState } from "react";
+import { Link, Router } from "wouter";
 import "./App.css";
 import ListOfGifs from "./components/ListOfGifs";
-
+import { Route } from "wouter";
 function App() {
   const [keyword, setKeyword] = useState("panda");
 
   return (
     <div className="App">
       <section className="App-content">
-        <button
-          onClick={() => {
-            setKeyword("Mapache");
-          }}
-        >
-          {" "}
-          Cambiar Keyword
-        </button>
-        <ListOfGifs keyword={keyword} />
-        <button>Cambiar Gifs</button>
+        <h1>App</h1>
+        <Link to="/gif/panda">Gif de pandas</Link>
+        <Link to="/gif/code">Gif de codigo</Link>
+        <Link to="/gif/argentina">Gif de argentina</Link>
+        <Link to="/gif/motores">Gif de motores</Link>
+        <Route path="/gif/:keyword" component={ListOfGifs} />
       </section>
     </div>
   );
--- Clase 2/giffy/src/components/ListOfGifs.js
+++ Clase 2/giffy/src/components/ListOfGifs.js
@@ -2,20 +2,21 @@ import React, { useEffect, useState } from "react";
 import Gif from "./Gif";
 import getGifs from "../services/getGifs";
 
-export default function ListOfGifs({ keyword }) {
+export default function ListOfGifs({ params }) {
   const [gifs, setGifs] = useState([]);
+  const {keyword} = params;
 
   useEffect(() => {
     console.log("Actualizando los gif");
     getGifs({ keyword }).then((gifs) => setGifs(gifs));
   }, [keyword]);
 
-  return gifs.map((singleGif) => (
-    <Gif
-      key={singleGif.id}
-      id={singleGif.id}
-      title={singleGif.title}
-      url={singleGif.url}
-    />
-  ));
+  return <div>
+    {gifs.map((singleGif) => (
+      <Gif 
+        key={singleGif.id} 
+        title={singleGif.title} 
+        url={singleGif.url} />
+    ))}
+  </div>;
 }
