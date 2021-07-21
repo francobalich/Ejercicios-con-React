598b2c89ae0fcd6d567f882f1a41d0ba9f5409f2
franco.balich@gmail.com <franco.balich@gmail.com>
2021-07-20 23:11:00 -0300

    Add browser input

--- Clase 3/giffy/src/pages/Home/index.js
+++ Clase 3/giffy/src/pages/Home/index.js
@@ -1,20 +1,36 @@
-import React from "react"
-import { Link } from "wouter"
-const POPULAR_GIFS=["Matrix","Code","Argentina","Panda"]
+import React, { useState } from "react";
+import { Link, useLocation } from "wouter";
+const POPULAR_GIFS = ["Matrix", "Code", "Argentina", "Panda"];
 
-export default function Home (){
-    
-    return (
-        <>
-            <h3 className="App.title">Los gifs más populares</h3>
-            <ul>
-                {POPULAR_GIFS.map((popularGif)=>(
-                    <li key={popularGif}>
-                        <Link to={`/search/${popularGif}`}>Gift de 
-                        {popularGif}</Link>
-                    </li>
-                ))}
-            </ul>
-        </>
-    )
+export default function Home() {
+  const [keyword, setKeyword] = useState("");
+  const [path,pushLocation] = useLocation()
+
+  const handleSubmit = (evt) => {
+      evt.preventDefault()
+    //nav to other rute
+    pushLocation(`/search/${keyword}`)
+    console.log(keyword);
+  };
+  const handleChange = (evt) => {
+    setKeyword(evt.target.value);
+  };
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <input onChange={handleChange} type="text" value={keyword} />
+      </form>
+      <h3 className="App.title">Los gifs más populares</h3>
+      <ul>
+        {POPULAR_GIFS.map((popularGif) => (
+          <li key={popularGif}>
+            <Link to={`/search/${popularGif}`}>
+              Gift de
+              {popularGif}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
